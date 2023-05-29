b7ea922aaceddb4e2f21bebde1ea089a79c50376
Haardik <ravat.1@iitj.ac.in>
2023-05-29 23:25:53 +0530

    App client changes

--- app_client/lib/homepage.dart
+++ app_client/lib/homepage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -13,11 +11,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-        Container(
-          child: const Text(
-            "hello"
-          )
-        )
+        Text(
+        "wear os"
+      )
     );
   }
 }
--- app_client/lib/main.dart
+++ app_client/lib/main.dart
@@ -1,31 +1,162 @@
-import 'package:app_client/homepage.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:is_wear/is_wear.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
+import 'package:watch_connectivity/watch_connectivity.dart';
+import 'package:wear/wear.dart';
 
-void main() {
+late final bool isWear;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  isWear = (await IsWear().check()) ?? false;
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final WatchConnectivityBase _watch;
+
+  // var _count = 0;
+
+  var _supported = false;
+  var _paired = false;
+  var _reachable = false;
+  // var _context = <String, dynamic>{};
+  // var _receivedContexts = <Map<String, dynamic>>[];
+  final _log = <String>[];
+
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _watch = WatchConnectivity();
+    _watch.messageStream
+        .listen((e) => setState(() => _log.add('Data Received: $e')));
+    initPlatformState();
+  }
+
+  void initPlatformState() async {
+    _supported = await _watch.isSupported;
+    _paired = await _watch.isPaired;
+    _reachable = await _watch.isReachable;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    final home = Scaffold(
+        appBar: AppBar(
+          title: const Text('Complex Behaviour Recognition'),
+        ),
+
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Supported: $_supported'),
+                Text('Paired: $_paired'),
+                Text('Reachable: $_reachable'),
+                TextButton(
+                  onPressed: initPlatformState,
+                  child: const Text('Refresh'),
+                ),
+                const SizedBox(height: 8),
+
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     TextButton(
+                //       onPressed: sendMessage,
+                //       child: const Text('Send Message'),
+                //     ),
+                //   ],
+                // ),
+                // TextButton(
+                //   onPressed: toggleBackgroundMessaging,
+                //   child: Text(
+                //     '${timer == null ? 'Start' : 'Stop'} background messaging',
+                //     textAlign: TextAlign.center,
+                //   ),
+                // ),
+                // const SizedBox(width: 16),
+                const Text('Received Data:'),
+                ..._log.reversed.map(Text.new),
+              ],
+            ),
+          ),
+        ),
       ),
-      home: const HomePage()
+    );
+
+    return MaterialApp(
+      home: isWear
+          ? AmbientMode(
+              builder: (context, mode, child) => child!,
+              child: home,
+            )
+          : home,
     );
   }
+
+//   void sendMessage() {
+//     final message = {'Gyro X': '0','Gyro Y': '0','Gyro Z': '0','Accel X': '0','Accel Y': '0','Accel Z': '0'};
+//     if(_reachable==true){
+//     _watch.sendMessage(message);
+//     setState(() => _log.add('Data: $message'));
+//   }}
+//
+//   // void sendContext() {
+//   //   _count++;
+//   //   final context = {'data': _count};
+//   //   _watch.updateApplicationContext(context);
+//   //   setState(() => _log.add('Sent context: $context'));
+//   // }
+//
+//   void toggleBackgroundMessaging() {
+//     if (timer == null) {
+//       timer = Timer.periodic(const Duration(seconds: 1), (_) => sendMessage());
+//     } else {
+//       timer?.cancel();
+//       timer = null;
+//     }
+//     setState(() {});
+//   }
+  void convertAndSaveToCSV(Map<String, dynamic> data) async {
+
+    List<List<dynamic>> csvData = [
+      // data.keys.toList(),
+      data.values.toList()
+    ];
+
+    String csvString = const ListToCsvConverter().convert(csvData);
+    Directory? directory = await getExternalStorageDirectory();
+
+    if (directory != null) {
+      File file = File('${directory.path}/file.csv');
+      await file.writeAsString(csvString);
+      OpenFile.open(file.path);
+    }
+    else{
+      print("Error saving");
+    }
+  }
+
+
+
 }
