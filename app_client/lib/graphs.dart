224d1baa0ed11942f16d2e5699f84a20de7c24f8
Dhruv-Mahajan <dhruv.mahajan001@gmail.com>
2023-06-20 13:44:51 +0530

    Add Sample Data Graph

--- app_client/lib/graphs.dart
+++ app_client/lib/graphs.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+// import 'dart:ffi';
 import 'dart:io';
-
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 import 'package:flutter/material.dart';
-
 
 class Graphs extends StatefulWidget {
   final List<dynamic> datalist;
@@ -14,22 +15,63 @@ class Graphs extends StatefulWidget {
 }
 
 class _GraphsState extends State<Graphs> {
-
-
-
   @override
   Widget build(BuildContext context) {
     print(widget.datalist);
+    List<_SensorData> data = [
+      _SensorData('1', 35, 40, 27),
+      _SensorData('2', 36, 41, 28),
+      _SensorData('3', 37, 42, 29),
+      _SensorData('4', 38, 43, 30),
+      _SensorData('5', 39, 44, 31),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Graphs'),
-      ), //AppBar
-      body: const Center(
-        child: Text(
-          'Page for graphs',
-
-        ), //Text
+      ),
+      body: Column(
+        children: [
+          SfCartesianChart(
+            primaryXAxis: CategoryAxis(),
+            title: ChartTitle(text: 'Accelerometer Data Analysis'),
+            legend: Legend(isVisible: true),
+            tooltipBehavior: TooltipBehavior(enable: true),
+            series: <ChartSeries<_SensorData, String>>[
+              LineSeries<_SensorData, String>(
+                dataSource: data,
+                xValueMapper: (_SensorData data, _) => data.time,
+                yValueMapper: (_SensorData data, _) => data.x,
+                name: 'Accelerometer-X',
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+              ),
+              LineSeries<_SensorData, String>(
+                dataSource: data,
+                xValueMapper: (_SensorData data, _) => data.time,
+                yValueMapper: (_SensorData data, _) => data.y,
+                name: 'Accelerometer-Y',
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+              ),
+              LineSeries<_SensorData, String>(
+                dataSource: data,
+                xValueMapper: (_SensorData data, _) => data.time,
+                yValueMapper: (_SensorData data, _) => data.z,
+                name: 'Accelerometer-Z',
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+}
+
+class _SensorData {
+  final String time;
+  final double x;
+  final double y;
+  final double z;
+
+  _SensorData(this.time, this.x, this.y, this.z);
 }
