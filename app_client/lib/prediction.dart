7fd933fc3bfb1206eb2bacb9b35e38d705c04af3
Haardik <ravat.1@iitj.ac.in>
2023-07-07 01:51:39 +0530

    Update prediction.dart

--- app_client/lib/prediction.dart
+++ app_client/lib/prediction.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'globals.dart' as g;
 import 'package:tflite/tflite.dart';
@@ -11,9 +13,46 @@ class ActivityRecog extends StatefulWidget {
 
 class _ActivityRecogState extends State<ActivityRecog> {
 
-
-
-
+var tflite=0;
+//
+// @override
+// void initState() {
+//   super.initState();
+//   _loading = true;
+//
+//   loadModel().then((value) {
+//     setState(() {
+//       _loading = false;
+//     });
+//   });
+// }
+//
+//
+// classifyImage(File image) async {
+//   var output = await Tflite.runModelOnImage(
+//     path: image.path,
+//     numResults: 2,
+//     threshold: 0.5,
+//     imageMean: 127.5,
+//     imageStd: 127.5,
+//   );
+//   setState(() {
+//     _loading = false;
+//     _outputs = output;
+//   });
+// }
+//
+// loadModel() async {
+//   await Tflite.loadModel(
+//     model: "assets/model_unquant.tflite",
+//     labels: "assets/labels.txt",
+//   );
+// }
+// @override
+// void dispose() {
+//   Tflite.close();
+//   super.dispose();
+// }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
